a51171c3be04c94e9b1e4caffcfaf64e25a562db
KennyCruzer <kennycruz@Kenneths-MBP.fios-router.home>
2019-11-06 16:44:35 -0500

    usestate, useEffect, useRef, and context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, createContext } from 'react';
+//use state cannot be used in a state component
+import Toggle from './Toggle';
+import { useTitleInput } from './hooks/useTitleInput';
+
+export const UserContext = createContext(); 
 
 const App = () => {
+  const [name, setName]  = useTitleInput('');
+  const ref = useRef();
+
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+    <UserContext.Provider
+      value={{
+        user: false
+      }}
+    >
+
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
+      <Toggle />
+      <form onSubmit={(e) => {
+        e.preventDefault(); 
+        
+      }}>
+        <input type="text" onChange={(e) => setName(e.target.value) } value={name} />
+        <button>Submit</button>
+      </form>
     </div>
+      </UserContext.Provider>
   );
 };
+
 
 export default App;
